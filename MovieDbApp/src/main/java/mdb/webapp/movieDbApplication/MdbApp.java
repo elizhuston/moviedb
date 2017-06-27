7fc4366f3563a9c0623cf905ba3bd3795b461079
Huston <n0270466@lm.lmig.com>
2017-06-27 11:30:54 -0400

    initial commit for movie db project

---
+++ MovieDbApp/src/main/java/mdb/webapp/movieDbApplication/MdbApp.java
@@ -0,0 +1,12 @@
+package mdb.webapp.movieDbApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdbApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdbApp.class, args);
+	}
+}
